d5b5063fea8f4e636248c9dcd3746f5aff91aaf5
Aubain <tapsoba.aubain@gmail.com>
2020-09-16 10:39:15 +0200

    Update setOrigin.js

--- setOrigin.js
+++ setOrigin.js
@@ -1,1 +1,10 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+       document.getElementById("demo").innerHTML = xhttp.responseText;
+    }
+};
+xhttp.open("GET", "https://hookb.in/VGP17oagG0TE22bwzRXn", true);
 xhr.setRequestHeader("Origin", 'maximum.blog');
+xhttp.send();
